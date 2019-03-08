edc56c62b7ca28fdbe8bdf63ae3437a40b0c922b
Gayatri Tavva <gayatri.tavva@gmail.com>
2019-03-08 15:37:36 -0800

    Css style changes

--- js/sales.js
+++ js/sales.js
@@ -3,6 +3,7 @@
 
 var locationHours =['6am','7am','8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm','4pm', '5pm', '6pm', '7pm', '8pm'];
 
+
 function SalamonCookieLocation(locationName,minCustomer,maxCustomer,averageCookiePerCustomer){
 this.locationName =locationName;
 this.minCustomer=minCustomer;
