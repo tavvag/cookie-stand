153108e0982359b49cc4532b58f039404a67f129
Gayatri Tavva <tavvag@amazon.com>
2019-03-22 01:06:55 -0700

    Forms and events lab8

--- js/sales.js
+++ js/sales.js
@@ -2,62 +2,140 @@
 
 
 var locationHours =['6am','7am','8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm','4pm', '5pm', '6pm', '7pm', '8pm'];
+var firstSubmit=false;
+var tableData =[];
+var totalLocations =0;
+var totalCookieSoldPerLocation=[];
+var form = document.getElementById('sales_form');
+document.getElementById('sales-table').style.visibility='hidden';
 
-
+//Constructor
 function SalamonCookieLocation(locationName,minCustomer,maxCustomer,averageCookiePerCustomer){
-this.locationName =locationName;
-this.minCustomer=minCustomer;
-this.maxCustomer=maxCustomer;
-this.averageCookiePerCustomer=averageCookiePerCustomer;
-
- this.averageCustomersPerHour =function () {
-     return Math.floor(Math.random() * (10)+20);
- }
- this.cookiesSoldPerHour =function () {
-    var averageCookiesSold= [];
-    for (var count= 0; count < locationHours.length; count++) {
-        averageCookiesSold.push(locationHours[count] +'-' +parseInt(this.averageCookiePerCustomer*this.averageCustomersPerHour()));
+  this.locationName =locationName;
+  this.minCustomer=minCustomer;
+  this.maxCustomer=maxCustomer;
+  this.averageCookiePerCustomer=averageCookiePerCustomer;
 }
-console.log(' Location '+  this.locationName + " Average Cookies Sold : "+ averageCookiesSold );
-return averageCookiesSold;
+//Generate Randam number
+SalamonCookieLocation.prototype.averageCustomersPerHour=function(){
+  return Math.floor(Math.random() * (this.maxCustomer-this.minCustomer)+20);
+};
+//Calculate cookies sold by hour
+SalamonCookieLocation.prototype.cookiesSoldPerHour=function(){
+  var averageCookiesSold= [];
+  for (var count= 0; count < locationHours.length; count++) {
+    averageCookiesSold.push(parseInt(this.averageCookiePerCustomer*this.averageCustomersPerHour()));
+  }
+  //console.log(' Location '+  this.locationName + " Average Cookies Sold : "+ averageCookiesSold );
+  return averageCookiesSold;
+};
+
+//Calculate cookies sold total per each location
+SalamonCookieLocation.prototype.totalCookiesSold=function(cookiesByHour){
+  var sum = 0;
+  //var toatalSales = this.cookiesSoldPerHour();
+  for(var i = 0; i < cookiesByHour.length; i++){
+    sum += cookiesByHour[i];
+  }
+  //console.log(' Location '+  this.locationName + " Total Sold : "+ sum );
+  return sum;
+};
+
+//Create td elements data
+function makeTable(locations,firstElement,lastElement){
+  var finalData ;
+  for(var i =0;i<locations.length;i++){
+    if(i===0){
+      finalData ='<th>'+firstElement+'</th>';
+    }
+    finalData = finalData+'<td>'+locations[i]+'</td>';
+  }
+  tableData.push(finalData+'<td>'+lastElement+'</td>');
+  //console.log('tableData in maketable'+ tableData);
 }
 
-this.totalCookiesSold=function(){
-    var sum = 0;
-    var toatalSales = this.cookiesSoldPerHour();
- for(var i = 0; i < toatalSales.length; i++){
-     var saleByHour = parseInt(toatalSales[i].substr(toatalSales[i].indexOf('-')+1,toatalSales[i].length)); 
-
- sum += saleByHour;
+//create a tr and append to parent table
+function renderTable(tableRow,elementId){
+  var newTableElement = document.getElementById(elementId);
+  for(var row=0;row<tableRow.length;row++){
+    var newTableRow = document.createElement('tr');
+    newTableRow.innerHTML=tableRow[row];
+    newTableElement.appendChild(newTableRow);
+  }
 
 }
-console.log(' Location '+  this.locationName + " Total Sold : "+ sum );
-    return sum;
+//Function will trigger on submit event
+function formSalesLocationData(event) {
+  event.preventDefault();
+  var allLocations=[];
+  var locationName = event.target.location.value;
+  var minCustomer = event.target.minimum.value;
+  var maxCustomer = event.target.maximum.value;
+  var averagePerCustomer = event.target.average.value;
+  totalLocations++;
+  allLocations.push(new SalamonCookieLocation(locationName, minCustomer, maxCustomer, averagePerCustomer));
+  printSalesData(allLocations);
+  if(totalLocations>1){
+    var footerEl = document.getElementById('footer');
+    while (footerEl.firstChild) {
+      // This will remove all children within tbody which in your case are <tr> elements
+      footerEl.removeChild(footerEl.firstChild);
+    }
+    printTotalsFooter();
+  }
+  form.reset();
 }
+//Print Sales Data for each location
+function printSalesData(location){
+  tableData =[];
+  //Create headers for table dynamically
+  if(firstSubmit===false){
+    document.getElementById('sales-table').style.visibility='visible';
+    makeTable(locationHours,' ','Daily Location Total');
+    renderTable(tableData,'heading');
+    firstSubmit=true;
+  }
+  for (var i = 0; i < location.length; i++) {
+    tableData =[];
+    var locationName = location[i].locationName;
+    var cookiesByHour = location[i].cookiesSoldPerHour();
+    var totalSalesByLoaction = location[i].totalCookiesSold(cookiesByHour);
+    makeTable(cookiesByHour,locationName,totalSalesByLoaction);
+    renderTable(tableData,'data');
+    totalCookieSoldPerLocation.push(cookiesByHour);
+  }
+
 }
 
-var pikeLocation = new SalamonCookieLocation("7thPike",40,200,2);
-var pierLocation = new SalamonCookieLocation("Pier99",50,250,2);
-var alaskanLocation = new SalamonCookieLocation("AlaskanWay",30,300,3);
-var seatacLocation = new SalamonCookieLocation("Seatac",100,500,5);
-var mercerLocation = new SalamonCookieLocation("Mercer Island",28,27,2);
-
-var allLocations = [pikeLocation,pierLocation,alaskanLocation,seatacLocation,mercerLocation];
-
-for (var i = 0; i < allLocations.length; i++) {
-    var ulElement = document.createElement('ul');
-    //ulElement.setAttribute("start", "8");
-  document.getElementById('location-name').appendChild(ulElement);
-  ulElement.innerHTML = allLocations[i].locationName;
-  var cookiesByHour = allLocations[i].cookiesSoldPerHour();
- for(var hour =0;hour<cookiesByHour.length;hour++){
-  var liElement = document.createElement('li');
-  var totalSales = allLocations[i].totalCookiesSold();
-  liElement.textContent = cookiesByHour[hour];
-  ulElement.appendChild(liElement);
- }
- var liTotalElement = document.createElement('li');
- var totalSales = allLocations[i].totalCookiesSold();
- liTotalElement.textContent = 'Total :' + totalSales;
- ulElement.appendChild(liTotalElement);
+//Print Sales Data for each location
+function printTotalsFooter(){
+  tableData =[];
+  var totalHourSales=[];
+  console.log('totalLocations :'+ totalLocations + '  totalCookieSoldPerLocation '+ totalCookieSoldPerLocation);
+  //Create Totals for each hour for all locations
+  for (var i = 0; i < locationHours.length; i++) {
+    var totalByHour=0;
+    for (var j = 0; j < totalLocations; j++){
+      var cookiesByHour = totalCookieSoldPerLocation[j];
+      totalByHour+=cookiesByHour[i];
+    }
+    totalHourSales.push(totalByHour);
+    console.log('totalHourSales :' + totalHourSales);
+  }
+  makeTable(totalHourSales,'Totals',grandTotal(totalHourSales));
+  renderTable(tableData,'footer');
 }
+
+function grandTotal(totalHourSales){
+  var total =0;
+  for(var i = 0; i < totalHourSales.length; i++){
+    total+=totalHourSales[i];
+  }
+  return total;
+}
+
+
+//Main Start of the program
+
+form.addEventListener('submit', formSalesLocationData);
+
